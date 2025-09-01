6da0465c287763f7b6965fb7592095fe9b5c8509
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-01 20:06:36 -0300

    feat: add create user controller with input validation and use case integration

---
+++ service-orders/src/http/controllers/user/create.ts
@@ -0,0 +1,26 @@
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+
+import { UserRepository } from '@/repositories/user.repository'
+import { CreateUserUseCase } from '@/use-cases/create-user'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    username: z.string().min(3),
+    password: z.string().min(6),
+  })
+
+  const { username, password } = registerBodySchema.parse(request.body)
+
+  try {
+    const userRepository = new UserRepository()
+    const createUserUseCase = new CreateUserUseCase(userRepository)
+
+    const user = await createUserUseCase.handler({ username, password })
+
+    return reply.status(201).send(user)
+  } catch (error) {
+    console.error(error)
+    return reply.status(500).send({ message: 'Internal Server Error' })
+  }
+}
