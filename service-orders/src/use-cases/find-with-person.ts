d0f9d63baade7f27c2943c088f07c9abad60ad8f
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-02 19:49:26 -0300

    feat: add FindWithPersonUseCase with dependency injection and error handling

--- service-orders/src/use-cases/find-with-person.ts
+++ service-orders/src/use-cases/find-with-person.ts
@@ -1,10 +1,10 @@
 import { Person } from '@/entities/person.entity'
 import { User } from '@/entities/user.entity'
-import { UserRepository } from '@/repositories/pg/user.repository'
+import type { IUserRepository } from '@/repositories/user.repository.interface'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 export class FindWithPersonUseCase {
-  constructor(private userRepository: UserRepository) {}
+  constructor(private userRepository: IUserRepository) {}
 
   async handler(userId: number): Promise<(User & Person) | undefined> {
     const user = await this.userRepository.findWithPerson(userId)
