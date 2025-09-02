bed8c1c20a2575c2dccb1b61670130e2529e2e52
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-02 15:40:54 -0300

    refactor: improve error handling using errorHandlerMap in global error handler

--- service-orders/src/utils/global-error-handler.ts
+++ service-orders/src/utils/global-error-handler.ts
@@ -4,23 +4,39 @@ import { ZodError } from 'zod'
 
 import { env } from '../env'
 
+interface ErrorHandlerMap {
+  [key: string]: (
+    error: Error | ZodError,
+    request: FastifyRequest,
+    reply: FastifyReply,
+  ) => void
+}
+
+export const errorHandlerMap: ErrorHandlerMap = {
+  ZodError: (error, _, reply) => {
+    return reply.status(400).send({
+      message: 'Validation error',
+      ...(error instanceof ZodError && { error: error.format() }),
+    })
+  },
+  ResourceNotFoundError: (error, _, reply) => {
+    return reply.status(404).send({ message: error.message })
+  },
+}
+
 export const globalErrorHandler = (
   error: Error,
   _: FastifyRequest,
   reply: FastifyReply,
 ) => {
-  if (error instanceof ZodError) {
-    return reply
-      .status(400)
-      .send({ message: 'Validation error', errors: error.format() })
-  }
-
-  if (error instanceof ResourceNotFoundError) {
-    return reply.status(404).send({ message: error.message })
-  }
-
   if (env.NODE_ENV !== 'development') {
     console.error(error)
+  }
+
+  const handler = errorHandlerMap[error.constructor.name]
+
+  if (handler) {
+    return handler(error, _, reply)
   }
 
   return reply.status(500).send({ message: 'Internal Server Error' })
