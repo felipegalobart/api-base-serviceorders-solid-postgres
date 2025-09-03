58f0f75e19f7f47b436f3f1b3eea02db0bdc4480
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-02 21:07:18 -0300

    feat: implement AddressRepository with findAddressByPersonId method using pagination and join with person

--- service-orders/src/repositories/address.repository.interface.ts
+++ service-orders/src/repositories/address.repository.interface.ts
@@ -1,11 +1,12 @@
 import type { Address } from '@/entities/address.entity'
+import type { Person } from '@/entities/person.entity'
 
 export interface IAddressRepository {
   findAddressByPersonId(
     personId: number,
     page: number,
     limit: number,
-  ): Promise<Address[]>
+  ): Promise<(Address & Person)[]>
 
   create(address: Address): Promise<Address | undefined>
 }
