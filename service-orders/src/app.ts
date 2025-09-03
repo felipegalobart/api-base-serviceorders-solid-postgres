47cbf75f191aa7f0b5fac51c64c4fae0c4a5e08b
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-03 19:18:47 -0300

    chore: setup TypeORM with reflect-metadata, tsconfig adjustments and typeorm config file

--- service-orders/src/app.ts
+++ service-orders/src/app.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata'
 import fastify from 'fastify'
 import { personRoutes } from '@/http/controllers/person/routes'
 import { userRoutes } from './http/controllers/user/routes'
