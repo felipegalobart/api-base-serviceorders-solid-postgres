02a180d7d0225381112f9fa375f8827a224d816f
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-04 15:51:23 -0300

    feat(category): implement create controller using Fastify and Zod for validation

--- service-orders/src/use-cases/factory/make-create-category-use-case.ts
+++ service-orders/src/use-cases/factory/make-create-category-use-case.ts
@@ -1,7 +1,7 @@
 import { CategoryRepository } from '@/repositories/typeorm/category.repository'
 import { CreateCategoryUseCase } from '../create-category'
 
-export function MakeCreateCategoryUseCase() {
+export function makeCreateCategoryUseCase() {
   const categoryRepository = new CategoryRepository()
   const createCategoryUseCase = new CreateCategoryUseCase(categoryRepository)
 
