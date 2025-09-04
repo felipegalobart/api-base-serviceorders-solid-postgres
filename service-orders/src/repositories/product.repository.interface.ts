d80e42c1c6e556467c11feec1d61c80fb1ce96e4
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-04 16:55:03 -0300

    fix(category): align column name in PostgreSQL to 'created_at' and update create controller to return success message

--- service-orders/src/repositories/product.repository.interface.ts
+++ service-orders/src/repositories/product.repository.interface.ts
@@ -2,4 +2,8 @@ import type { IProduct } from '@/entities/models/product.interface'
 
 export interface IProductRepository {
   create: (product: IProduct) => Promise<IProduct>
+  findAll: (page: number, limit: number) => Promise<IProduct[]>
+  findById: (id: string) => Promise<IProduct | null>
+  update: (product: IProduct) => Promise<IProduct>
+  delete: (id: string) => Promise<void>
 }
