8dcf086cf4633781da946369c9f003e3d49ffc9d
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-04 19:45:50 -0300

    feat(product): add CRUD routes for product resource

--- service-orders/src/http/controllers/product/routes.ts
+++ service-orders/src/http/controllers/product/routes.ts
@@ -1,6 +1,14 @@
 import type { FastifyInstance } from 'fastify'
 import { create } from './create'
+import { findProduct } from './find-product'
+import { findAllProducts } from './find-all-products'
+import { deleteProduct } from './delete'
+import { update } from './update'
 
 export async function productRoutes(app: FastifyInstance) {
   app.post('/product', create)
+  app.get('/product/:id', findProduct)
+  app.get('/product', findAllProducts)
+  app.delete('/product/:id', deleteProduct)
+  app.put('/product/:id', update)
 }
