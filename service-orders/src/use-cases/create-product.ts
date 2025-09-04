4585750a6e37993aca26687a5fa635e48bc391f4
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-03 21:02:02 -0300

    feat: add CreateProductUseCase with handler method using IProductRepository

---
+++ service-orders/src/use-cases/create-product.ts
@@ -0,0 +1,10 @@
+import type { IProduct } from '@/entities/models/product.interface'
+import type { IProductRepository } from '@/repositories/product.repository.interface'
+
+export class CreateProductUseCase {
+  constructor(private productRepository: IProductRepository) {}
+
+  async handler(product: IProduct): Promise<IProduct> {
+    return this.productRepository.create(product)
+  }
+}
