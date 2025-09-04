368b034297f32e2e884da9b4b21bffa45636ccd0
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-03 21:39:25 -0300

    fix(product): assert categories as ICategory[] to satisfy strict type checks

---
+++ service-orders/src/http/controllers/product/create.ts
@@ -0,0 +1,31 @@
+import type { ICategory } from '@/entities/models/category.interface'
+import { makeCreateProductUseCase } from '@/use-cases/factory/make-create-product-use-case'
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+
+export async function create(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    description: z.string(),
+    image_url: z.string(),
+    price: z.coerce.number(),
+    categories: z
+      .array(z.object({ id: z.coerce.number().optional(), name: z.string() }))
+      .optional(),
+  })
+
+  const { name, description, image_url, price, categories } =
+    registerBodySchema.parse(request.body)
+
+  const createProductUseCase = makeCreateProductUseCase()
+
+  const product = await createProductUseCase.handler({
+    name,
+    description,
+    image_url,
+    price,
+    categories: categories as ICategory[],
+  })
+
+  return reply.status(201).send(product)
+}
