3dda843977ec079834c96c1be56d8b9b3f96d97a
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-05 16:30:03 -0300

    Implement signin controller with JWT authentication and password validation

---
+++ service-orders/src/http/controllers/user/signin.ts
@@ -0,0 +1,30 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeSignInUseCase } from '@/use-cases/factory/make-signin-use-case'
+import { compare } from 'bcryptjs'
+import type { FastifyRequest, FastifyReply } from 'fastify'
+import z from 'zod'
+
+export async function signin(request: FastifyRequest, reply: FastifyReply) {
+  const registerBodySchema = z.object({
+    username: z.string().min(3),
+    password: z.string().min(6),
+  })
+
+  const { username, password } = registerBodySchema.parse(request.body)
+
+  const signInUseCase = makeSignInUseCase()
+
+  const user = await signInUseCase.handler(username)
+
+  const isPasswordCorrect = await compare(password, user.password)
+
+  if (!isPasswordCorrect) {
+    throw new InvalidCredentialsError()
+  }
+
+  const token = await reply.jwtSign({ username })
+
+  return reply.status(200).send({
+    token,
+  })
+}
