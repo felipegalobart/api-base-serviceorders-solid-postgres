6cbff6a992fbe3beeb8d92d3fc812134ede40d3e
Felipe Galobart Jora <felipegalobart@outlook.com>
2025-09-08 15:15:06 -0300

    feat(user): refactor User entity to use TypeORM decorators and add timestamps

--- service-orders/src/entities/user.entity.ts
+++ service-orders/src/entities/user.entity.ts
@@ -1,12 +1,30 @@
-import type { IUser } from './models/user.interface'
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm'
 
-export class User implements IUser {
-  id?: number
+@Entity('user')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ unique: true })
   username: string
+
+  @Column()
   password: string
 
-  constructor(username: string, password: string) {
-    this.username = username
-    this.password = password
+  @CreateDateColumn()
+  created_at: Date
+
+  @UpdateDateColumn()
+  updated_at: Date
+
+  constructor(username?: string, password?: string) {
+    if (username) this.username = username
+    if (password) this.password = password
   }
 }
